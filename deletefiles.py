f924defda1557662c851b2025ea7eef1df8fd7cb
vijayamurugan <vijayamurugan@users.noreply.github.com>
2017-12-26 12:50:36 +0530

    Added an option to delete a file, folder with contents & empty folder

--- deletefiles.py
+++ deletefiles.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 import os, time, sys
+import shutil
 
 def deleteFile(days,path):
-   print(path)
-   now = time.time()
-   for f in os.listdir(path):
+  input_args = locals()
+  print("input arguments are", input_args)
+  now = time.time()
+  for f in os.listdir(path):
     f = os.path.join(path, f) 
     if os.stat(f).st_mtime < now - int(days) * 86400:
-     if os.path.isfile(f):
-       os.remove(f)
+     print(f)
+     shutil.rmtree(f)
+     os.remove(f)
+     os.rmdir(f)
+     
      
 params=sys.argv;
 if len(params)>1:
  deleteFile (params[1],params[2])
 else:
  print("Missing arguments. i.e days and path")
-
-
-    
